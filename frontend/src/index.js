5ef8cbece284e42fa630ee7da4a5c9d4ca3ffb13
Aman Kumar <amankr2702@gmail.com>
2021-07-10 17:08:50 +0530

    Add index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+//Importing the dependencies and required components
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
